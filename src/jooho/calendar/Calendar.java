2773b1ad727482f8a21594d1843d0634fa8ccc63
ParkJooHo <juho2075@naver.com>
2022-11-01 14:43:31 +0900

    Update finding days of the month3

--- src/jooho/calendar/Calendar.java
+++ src/jooho/calendar/Calendar.java
@@ -3,19 +3,13 @@ package jooho.calendar;
 import java.util.Scanner;
 
 public class Calendar {
-	
-	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 	public int getMaxDaysOfMonth(int month) { // "maxDaysOfMonth"를 함수로 빼준 것
-		switch (month) {
-		case 2:
-			return 28;
-		case 4:
-			return 30;
-		default:
-			return 31;
+		return MAX_DAYS[month - 1];
 		}
-	}
-	
+
 	public void printSampleCalendar() {
 		System.out.println(" 일  월  화  수  목  금  토");
 		System.out.println("-----------------------");
@@ -26,20 +20,20 @@ public class Calendar {
 	}
 
 	public static void main(String[] args) {
-		
+
 		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 		System.out.println("달을 입력하세요 : ");
 		int month = scanner.nextInt();
-		
-		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-		
+
+		int[] maxDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 		System.out.printf("%d 월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
 		// "cal.maxDaysOfMonth"를 함수 또는 메소드라고 부름
-		
+
 		cal.printSampleCalendar();
-		
+
 		scanner.close();
 	}
 }
